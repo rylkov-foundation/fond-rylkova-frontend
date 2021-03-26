74f8087e81eb72c962fd0081dcfdc182c6c3986a
freebeego <freebeego@yandex.ru>
2021-03-26 15:40:29 +0300

    build: add eslint with nuxt recommended rules

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,6 +28,7 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    '@nuxtjs/eslint-module'
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
