529fedd05f5e2f7860694bd7c82527ccade3de8e
freebeego <freebeego@yandex.ru>
2021-03-29 15:43:23 +0300

    fix(Result): change font-size and line-height to css lock

--- components/Result.vue
+++ components/Result.vue
@@ -19,94 +19,100 @@ export default {
 </script>
 
 <style scoped>
-.results__result {
-  margin-bottom: 20px;
-}
-
-.result {
-  display: grid;
-  grid-template-areas:
-    "image title"
-    "image text";
-  grid-template-columns: 142px auto;
-  overflow: hidden;
-  position: relative;
-}
-
-.result__image {
-  grid-area: image;
-  height: 197px;
-  position: relative;
-  left: -55px;
-}
-
-.result__number {
-  font-family: Tomorrow, Times, fantasy;
-  font-size: 115px;
-  line-height: 83px;
-  position: absolute;
-  top: 30%;
-  left: 15px;
-  color: #fff;
-}
-
-.result__title {
-  grid-area: title;
-  color: #b23438;
-  font-family: Vollkorn, Times, serif;
-  font-size: 28px;
-  line-height: 25px;
-  text-transform: uppercase;
-  border-bottom: 2px solid #fff;
-  margin: 12px 0 0 3px;
-  max-width: 91%;
-  align-self: start;
-}
-
-.result__text {
-  grid-area: text;
-  align-self: start;
-  font-family: Vollkorn, Times, serif;
-  font-size: 14px;
-  font-weight: 400;
-  line-height: 15px;
-  margin-left: 5px;
-}
-
-@media screen and (min-width: 768px) {
   .results__result {
-    margin-bottom: 15px;
+    margin-bottom: 20px;
   }
 
   .result {
-    grid-template-columns: 379px auto;
+    display: grid;
+    grid-template-areas:
+      "image title"
+      "image text";
+    grid-template-columns: 142px auto;
+    overflow: hidden;
+    position: relative;
   }
 
   .result__image {
-    height: 416px;
-    left: -60px;
+    grid-area: image;
+    height: 197px;
+    position: relative;
+    left: -55px;
   }
 
   .result__number {
-    font-size: 279px;
-    line-height: 200px;
-    top: 27%;
-    left: 55px;
+    font-family: Tomorrow, Times, fantasy;
+    font-size: 115px;
+    line-height: 83px;
+    position: absolute;
+    top: 30%;
+    left: 15px;
+    color: #fff;
   }
 
   .result__title {
-    font-size: 64px;
-    line-height: 60px;
+    grid-area: title;
+    color: #b23438;
+    font-family: Vollkorn, Times, serif;
+    font-size: 28px;
+    line-height: 25px;
+    text-transform: uppercase;
     border-bottom: 2px solid #fff;
-    padding-bottom: 25px;
-    margin: 0 0 0 3px;
-    max-width: 360px;
+    margin: 12px 0 0 3px;
+    max-width: 91%;
+    align-self: start;
   }
 
   .result__text {
-    font-size: 30px;
-    line-height: 30px;
+    grid-area: text;
+    align-self: start;
+    font-family: Vollkorn, Times, serif;
+    font-size: calc(0.035714286 * 100vw + 2.57142848px);
+    font-weight: 400;
+    line-height: calc(0.033482143 * 100vw + 4.28571424px);
     margin-left: 5px;
   }
-}
+
+  @media screen and (min-width: 440px) {
+    .result__title {
+      text-align: center;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .results__result {
+      margin-bottom: 15px;
+    }
+
+    .result {
+      grid-template-columns: 379px auto;
+    }
+
+    .result__image {
+      height: 416px;
+      left: -60px;
+    }
+
+    .result__number {
+      font-size: 279px;
+      line-height: 200px;
+      top: 27%;
+      left: 55px;
+    }
+
+    .result__title {
+      font-size: 64px;
+      line-height: 60px;
+      border-bottom: 2px solid #fff;
+      padding-bottom: 25px;
+      margin: 0 0 0 3px;
+      max-width: 360px;
+    }
+
+    .result__text {
+      font-size: 30px;
+      line-height: 30px;
+      margin-left: 5px;
+    }
+  }
 </style>
