73306736b2c6b1672ef15453d0c31e9be202c95a
freebeego <freebeego@yandex.ru>
2021-03-29 18:00:35 +0300

    fix: add Roboto font

---
+++ components/Footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer class="footer">
+    <h2 class="footer__title">мы в соцсетях</h2>
+    <p class="footer__foreign-agent"></p>
+    <p class="footer__license"></p>
+    <p class="footer__social-links"></p>
+    <p class="footer__copyright"></p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
+
+<style scoped>
+  .footer {
+    display: flex;
+  }
+
+  .footer__title {
+    text-transform: uppercase;
+    color: #727272;
+    font-family: Roboto, sans-serif;
+    font-size: 11px;
+    line-height: 19px;
+  }
+</style>
