26b9400bd1a1d077b46f6b818d002b8868338612
Natalia Popova <cindy.n-popova@yandex.ru>
2021-04-15 20:46:12 +0300

    feat(reports): add annual-report page, reports component incl report component + layout for 320px

---
+++ components/Report.vue
@@ -0,0 +1,114 @@
+<template>
+  <li
+    class="report"
+    :class="{ 'report_color_white': ($attrs.index+1)%2 === 0}"
+  >
+    <img
+      :src="require(`../assets/images/${report.imageLink}`)"
+      :alt="report.title"
+      class="report__image"
+    >
+    <div class="report__text-container">
+      <h3
+        class="report__title"
+        :class="{ 'report__title_color_white': ($attrs.index+1)%2 === 0}"
+      >
+        {{ report.title }}
+      </h3>
+      <p
+        class="report__date"
+        :class="{ 'report__date_color_white': ($attrs.index+1)%2 === 0}"
+      >
+        {{ report.date }}
+      </p>
+      <p
+        class="report__text"
+        :class="{ 'report__text_color_white': ($attrs.index+1)%2 === 0}"
+      >
+        {{ report.text }}
+      </p>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'Report',
+  props: {
+    report: {
+      type: Object,
+      default: () => {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .report {
+    display: flex;
+    flex-direction: column;
+    background-color: #b23438;
+    border-bottom: 1px solid #fff;
+    align-items: center;
+  }
+
+  .report_color_white {
+    background-color: #fff;
+    border-color: #434343;
+  }
+
+  .report__image {
+    width: 320px;
+    height: 190px;
+    object-fit: cover;
+  }
+
+  .report__text-container {
+    display: flex;
+    flex-direction: column;
+    max-width: 83%;
+    margin: 7px auto 28px;
+  }
+
+  .report__title {
+    font-family: Vollkorn, Times, serif;
+    font-size: 23px;
+    line-height: 23px;
+    text-transform: uppercase;
+    color: #c8c8c8;
+    margin-bottom: 13px;
+    font-weight: 600;
+  }
+
+  .report__title_color_white {
+    color: #b23438;
+  }
+
+  .report__date {
+    font-family: Vollkorn, Times, serif;
+    font-size: 16px;
+    line-height: 13px;
+    color: #000;
+    font-weight: 600;
+    border-bottom: 2px solid #fff;
+    align-self: flex-start;
+    padding-bottom: 7px;
+    margin-bottom: 7px;
+  }
+
+  .report__date_color_white {
+    border-color: #000;
+  }
+
+  .report__text {
+    font-family: Vollkorn, Times, serif;
+    font-size: 13px;
+    line-height: 16px;
+    color: #fff;
+    font-weight: 400;
+  }
+
+  .report__text_color_white {
+    color: #615858;
+  }
+</style>
---
+++ components/Reports.vue
@@ -0,0 +1,152 @@
+<template>
+  <section class="reports">
+    <div class="reports__container">
+      <h2 class="reports__title">
+        Финансовые и содержательные годовые  отчеты
+        <span class="reports__line reports__line_number_tree" />
+        <span class="reports__line reports__line_number_four" />
+      </h2>
+      <p class="reports__subtitle">
+        В данном разделе представлена  информация о реализующихся  в настоящее время проектах фонда
+      </p>
+      <Bolt class="reports__image" />
+    </div>
+    <ul class="reports__list">
+      <Report
+        v-for="(report,index) in reportsData"
+        :key="report.id"
+        :report="report"
+        :index="index"
+      />
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Reports',
+  data () {
+    return {
+      reportsData: [
+        {
+          id: '1',
+          imageLink: 'reports-picture1.png',
+          title: 'Отчет о работе  проекта  “Снижение вреда –  Москва” в 2018 году',
+          date: 'Январь 16th, 2020',
+          text: 'Проект реализовывался ФАР при поддержке AIDSFonds'
+        },
+        {
+          id: '2',
+          imageLink: 'reports-picture-2.png',
+          title: 'Отчет о  работе проекта  “Снижение вреда –  Москва” в 2018 году',
+          date: 'Январь 16th, 2020',
+          text: 'Мы собрали самые важные данные, которые отражают проделанную нами работу за 2018 год, и сделали красивую инфографику'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .reports {
+    overflow: hidden;
+    position: relative;
+    width: 100vw;
+  }
+
+  .reports__container {
+    min-height: 300px;
+  }
+
+  .reports__title {
+    font-family: Vollkorn, Times, serif;
+    font-size: 40px;
+    line-height: 33px;
+    font-weight: 700;
+    width: 83%;
+    max-width: 415px;
+    word-break: break-all;
+    margin: 32px 0 20px 12px;
+    position: relative;
+  }
+
+  .reports__title::before {
+    content: "";
+    height: 12px;
+    background-color: #b23438;
+    width: 90%;
+    position: absolute;
+    top: 23px;
+    left: 0;
+  }
+
+  .reports__title::after {
+    content: "";
+    height: 12px;
+    background-color: #b23438;
+    width: 100%;
+    position: absolute;
+    top: 55px;
+    left: 0;
+  }
+
+  .reports__line {
+    content: "";
+    height: 12px;
+    background-color: #b23438;
+    position: absolute;
+  }
+
+  .reports__line_number_tree {
+    top: 92px;
+    left: 0;
+    width: 94%;
+  }
+
+  .reports__line_number_four {
+    top: 119px;
+    left: 0;
+    width: 75%;
+  }
+
+  .reports__subtitle {
+    font-family: Vollkorn, Times, serif;
+    font-size: 15px;
+    line-height: 17px;
+    font-weight: 400;
+    max-width: 43%;
+    margin: 0 0 0 33px;
+  }
+
+  .reports__image {
+    position: absolute;
+    right: -32px;
+    top: 136px;
+    width: 182px;
+    height: 174px;
+  }
+
+  .reports__list {
+    list-style: none;
+    padding: 0;
+  }
+
+  @media screen and (min-width: 354px) {
+    .reports__line_number_four {
+      width: 49%;
+    }
+  }
+
+  @media screen and (min-width: 354px) {
+    .reports__line_number_four {
+      display: none;
+    }
+  }
+
+  @media screen and (min-width: 512px) {
+    .reports__line_number_tree {
+      width: 36%;
+    }
+  }
+</style>
