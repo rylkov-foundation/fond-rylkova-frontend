c61f132508393125e6dd87ff691624117dbfd8fe
freebeego <freebeego@yandex.ru>
2021-05-18 23:23:26 +0300

    fix: local dictionary property

---
+++ lang/en.js
@@ -0,0 +1,51 @@
+export default {
+  links: {
+    donate: 'Donate',
+    getHelp: 'Get help',
+    moreAboutUs: 'More about us',
+    moreDetailed: 'More detailed',
+    privacyPolicy: 'Privacy Policy'
+  },
+  donateForm: {
+    once: 'Once',
+    regularly: 'Regularly',
+    card: 'Card',
+    cash: 'Cash',
+    yooMoney: 'ЮMoney',
+    otherAmount: 'Other amount',
+    agreeWith: 'I agree with',
+    offer: 'the offer',
+    name: 'Name',
+    email: 'Email',
+    submitButtonText: 'Donate',
+    validationMessages: {
+      minAmount: 'Minimum amount is {minAmount} rubles',
+      needAcceptOffer: 'Need to accept the offer',
+      needFillFields: 'Need to fill name and email'
+    }
+  },
+  support: {
+    buttonText: 'Donate'
+  },
+  emailForm: {
+    subject: 'subject',
+    name: 'Name',
+    email: 'E-mail',
+    submitButtonText: 'Send'
+  },
+  requisites: {
+    title: 'Requisites',
+    companyName: 'Company name',
+    companyShortName: 'Short company name',
+    bankRequisites: 'Bank requisites',
+    inn: 'Taxpayer Identification Number (INN)',
+    kpp: 'Tax Registration Reason Code (KPP)',
+    ogrn: 'Primary State Registration Number (OGRN)',
+    correspondentAccount: 'Corr. account',
+    bankAccountNumber: 'Bank account number',
+    bankAccountRuble: 'Ruble bank account',
+    bankName: 'Bank name',
+    bik: 'RCBIC',
+    purpose: 'Purpose of payment'
+  }
+}
---
+++ lang/ru.js
@@ -0,0 +1,51 @@
+export default {
+  links: {
+    donate: 'Поддержать',
+    getHelp: 'Получить помощь',
+    moreAboutUs: 'Подробнее о нас',
+    moreDetailed: 'Подробнее',
+    privacyPolicy: 'Политика конфеденциальности'
+  },
+  donateForm: {
+    once: 'Однократно',
+    regularly: 'Регулярно',
+    card: 'Карта',
+    cash: 'Терминал',
+    yooMoney: 'ЮMoney',
+    otherAmount: 'Другая сумма',
+    agreeWith: 'Согласен с',
+    offer: 'офертой',
+    name: 'Имя',
+    email: 'Email',
+    submitButtonText: 'Перейти к оплате',
+    validationMessages: {
+      minAmount: 'Минимальная сумма {minAmount} рублей',
+      needAcceptOffer: 'Необходимо принять оферту',
+      needFillFields: 'Необходимо ввести имя и email'
+    }
+  },
+  support: {
+    buttonText: 'Поддержать'
+  },
+  emailForm: {
+    subject: 'тема',
+    name: 'Имя',
+    email: 'E-mail',
+    submitButtonText: 'Отправить'
+  },
+  requisites: {
+    title: 'Реквизиты',
+    companyName: 'Название организации',
+    companyShortName: 'Сокращенно',
+    bankRequisites: 'Банковские реквизиты',
+    inn: 'ИНН',
+    kpp: 'КПП',
+    ogrn: 'ОГРН',
+    correspondentAccount: 'КОР. СЧЕТ',
+    bankAccountNumber: 'РАСЧЕТНЫЙ СЧЕТ',
+    bankAccountRuble: 'Расчетный счет в рублях РФ',
+    bankName: 'НАИМЕНОВАНИЕ БАНКА',
+    bik: 'БИК',
+    purpose: 'НАЗНАЧЕНИЕ ПЛАТЕЖА'
+  }
+}
